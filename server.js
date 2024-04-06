529aa09923823f897e65eaa70ec7c9b7d587a96f
josglenn <josglenn123@gmail.com>
2024-04-06 10:56:47 +0800

    initial commit

---
+++ server.js
@@ -0,0 +1,30 @@
+import { createServer } from "http";
+import { Server } from "socket.io";
+import Express from "express";
+
+const app = Express();
+app.use(Express.json());
+
+const PORT = process.env.PORT || 4000;
+
+const expressServer = app.listen(PORT, () => {
+  console.log(`Server listening on porrt`);
+});
+
+const io = new Server(expressServer, {
+  cors: {
+    origin:
+      process.env.PORT === "production"
+        ? false
+        : ["http://localhost:3000", "http://127.0.0.1:3000"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User ${socket.id} connected`);
+
+  socket.on("message", (message) => {
+    console.log(message);
+    socket.emit("message", message);
+  });
+});
